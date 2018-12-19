8abdca51e16089763949884613f511e0de905cbf
nabehide <iasoyv@gmail.com>
2018-12-19 16:45:16 +0900

    mod: navigation

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -1,8 +1,5 @@
 <template>
   <div class="container">
-    <div class="canvasContainer">
-      <GlslCanvas class="canvas" />
-    </div>
     <nav :class="{ 'isColorInverted' : (isColorInverted === true) }">
       <ul>
         <nuxt-link exact to="/" ><li>Top</li></nuxt-link>
@@ -14,10 +11,8 @@
 </template>
 
 <script>
-import GlslCanvas from './GlslCanvas'
 export default {
   components: {
-    GlslCanvas,
   },
   computed: {
     isColorInverted () { return this.$store.state.parameters.invertColor.isColorInverted.isColorInverted }
@@ -55,20 +50,6 @@ nav {
     style: solid;
     width: thin;
   }
-}
-canvasContainer {
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-}
-.canvas {
-  z-index: -100;
-
-  position: fixed;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
 }
 nav.isColorInverted {
   ul {
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,15 +1,20 @@
 <template>
   <div class="container">
+    <div class="canvasContainer">
+      <GlslCanvas class="canvas" />
+    </div>
     <Navigation />
     <nuxt />
   </div>
 </template>
 
 <script>
+import GlslCanvas from '~/components/GlslCanvas'
 import Navigation from '~/components/Navigation'
 
 export default {
   components: {
+    GlslCanvas,
     Navigation,
   }
 }
@@ -18,5 +23,19 @@ export default {
 <style scoped lang="scss">
 .container {
   margin: 5px;
+}
+canvasContainer {
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+}
+.canvas {
+  z-index: -100;
+
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
 }
 </style>
