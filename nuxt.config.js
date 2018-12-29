8fd913909be82dbe6ea6e8645e95163b17732840
nabehide <iasoyv@gmail.com>
2018-12-29 20:10:16 +0900

    ogp

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,7 +10,17 @@ module.exports = {
   head: {
     title: 'nabehide\'s portfolio',
     meta: [
+      { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'This site is created by myself using WebGL(Three.js), Nuxt.js and GLSL.' },
+      { property: 'og:title', content: 'nabehide portfolio' },
+      { property: 'og:type', content: 'website' },
+      { property: 'og:url', content: 'https://nabehide.github.io' },
+      { property: 'og:description', content: 'This site is created with Nuxt.js and Three.js(WebGL/GLSL).' },
+      { property: 'og:image', content: 'https://nabehide.github.io/thumnail.png' },
+
+      { property: 'og:twitter:card', content: 'card' },
+      { property: 'og:twitter:site', content: '@____nabehide' },
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
