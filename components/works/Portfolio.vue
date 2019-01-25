662a5b064e0aea70a3e977cf0eed3cd0655dbb87
nabehide <iasoyv@gmail.com>
2019-01-25 23:27:33 +0900

    mod: introduce portfolio with audio effect

--- components/works/Portfolio.vue
+++ components/works/Portfolio.vue
@@ -3,12 +3,14 @@
     "ja": {
       "title": "ポートフォリオ（このサイト）",
       "description1": "フロントエンドはNuxt.js, THREE.js(WebGL, GLSL)で作成しています。",
+      "description2": "Web Audio APIを用いたAudio effectが追加されたバージョンもあります。",
 
       "site": "サイト"
     },
     "en": {
       "title": "Portfolio(this site)",
       "description1": "The website is created with Nuxt.js and Three.js(WebGL, GLSL).",
+      "description2": "Another version with audio effect using Web Audio API (music, microphone) is available.",
 
       "site": "site"
     }
@@ -20,9 +22,15 @@
     <h3>{{ $t('title') }}</h3>
     <div class="descriptions">
       <p>{{ $t('description1') }}</p>
+      <p>{{ $t('description2') }}</p>
       <p>
         <a href="https://nabehide.github.io" target="_blank">
           <font-awesome-icon :icon="['fas', 'globe']" /> Web{{ $t('site') }}
+        </a>
+      </p>
+      <p>
+        <a href="https://nabehide.github.io/portfolio_with_audio_effect" target="_blank">
+          <font-awesome-icon :icon="['fas', 'globe']" /> Web{{ $t('site') }} with audio effect (PC recommended)
         </a>
       </p>
     </div>
